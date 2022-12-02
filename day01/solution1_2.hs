58d60bcf6ea6c67d68d05f8d2227ca0b5f06a1ac
Marcus Xavier <marcus.xavier@aluno.ufop.edu.br>
2022-12-01 21:23:33 -0300

    second puzzle done

---
+++ day01/solution1_2.hs
@@ -0,0 +1,30 @@
+import System.IO
+import Control.Monad
+import Text.Read (readMaybe)
+import Data.Maybe (fromJust)
+import Data.List (sort)
+
+group' :: Eq a => (a -> Bool) -> [a] -> [[a]]
+group' _ [] = []
+group' f xs = takeWhile f xs : group' f ((safeTail . dropWhile f) xs)
+
+safeTail :: [a] -> [a]
+safeTail [] = []
+safeTail (x:xs) = xs
+
+readMaybeInt :: String -> Maybe Int
+readMaybeInt = readMaybe
+
+maybee :: [String] -> Maybe Int
+maybee = fmap sum . sequence . map readMaybeInt
+
+mapJust :: String -> [Maybe Int]
+mapJust = map maybee . group' (/= "") . lines
+
+main :: IO ()
+main = do
+   handle <- openFile "./data" ReadMode
+   content <- hGetContents handle
+   let max = (fmap sum . sequence . take 3 . reverse . sort . mapJust) content
+   print $ fromJust max
+   hClose handle
